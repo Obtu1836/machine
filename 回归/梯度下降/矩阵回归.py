15a53eba3aaa513a6e9d0bc6f63f9d85ea94a83e
Obtu1836 <ss751157ss@163.com>
2024-09-21 11:24:06 +0800

    dequec处理循环结束

--- 回归/梯度下降/矩阵回归.py
+++ 回归/梯度下降/矩阵回归.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import deque
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
@@ -20,21 +21,27 @@ if __name__ == '__main__':
     x,y=make_regression(500,3)
     x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.7,random_state=0)
     m,n=x_train.shape
+    dq=deque(maxlen=5)
 
     w=np.zeros((n,1))
     lr=0.001
     los=loss(x_train,w,y_train)
+    dq.append(los)
+
     while True:
         w=w-lr*grad(x_train,w,y_train)
         new_los=loss(x_train,w,y_train)
-        if np.allclose(los,new_los):
+
+        dq.append(new_los)
+        rs=np.array(dq)
+
+        if np.allclose(rs,rs.mean(keepdims=1)):
             break
-        los=new_los
 
     y_p=x_test.dot(w)
 
-    flag=np.allclose(y_p.ravel(),y_test)
-    print(flag)
+    print(np.allclose(y_p.ravel(),y_test,atol=1e-2))
+
 
 
     
