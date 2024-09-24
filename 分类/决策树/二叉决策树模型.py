23da01529d2e6da2ac43174bf3b84bcf291a22d9
Obtu1836 <ss751157ss@163.com>
2024-09-24 17:34:44 +0800

    聚类完成后的标签和真实标签的对应关系建立

---
+++ 分类/决策树/二叉决策树模型.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+
+def cal_info_gini(data, mode):
+
+    label = data[:, -1]
+    counts = np.unique(label, return_counts=1)[1]
+    p = counts/counts.sum()
+
+    if mode == 'info':
+        return (-p*np.log2(p)).sum()
+    else:
+        return 1-np.power(p, 2)
+
+
+def split(data, col, val):
+
+    con = data[:, col] < val
+
+    l_data = data[con]
+    r_data = data[~con]
+
+    return l_data, r_data
+
+
+class Tree:
+    def __init__(self, col=-1, val=None, sign=None,
+                 l=None, r=None, mode=None):
+
+        self.col = col
+        self.val = val
+        self.sign = sign
+        self.l = l
+        self.r = r
+        self.mode = mode
+
+
+def build(data, mode):
+
+    if len(data) == 0:
+        return Tree()
+
+    basic = 0
+    init_info_gini = cal_info_gini(data, mode)
+
+    n = data.shape[1]-1
+
+    for col in range(n):
+        for val in data[:, col]:
+
+            l_data, r_data = split(data, col, val)
+
+            l_info_gini = cal_info_gini(l_data, mode)*len(l_data)
+            r_info_gini = cal_info_gini(r_data, mode)*len(r_data)
+
+            new_info_gini = (l_info_gini+r_info_gini)/len(data)
+
+            ins = init_info_gini-new_info_gini
+
+            if ins > basic and len(l_data) > 0 and len(r_data) > 0:
+
+                basic = ins
+                best_col = col
+                best_val = val
+                best_l = l_data
+                best_r = r_data
+
+    if basic > 0:
+        l = build(best_l, mode)
+        r = build(best_r, mode)
+
+        return Tree(col=best_col, val=best_val, l=l, r=r, mode=mode)
+
+    else:
+
+        lab, cuns = np.unique(data[:, -1], return_counts=1)
+        sign = lab[np.argmax(cuns)]
+        return Tree(sign=sign, mode=mode)
+    
+def printf(tree,level='o'):
+
+    if tree.sign!=None:
+        print(level+'-'+str(tree.sign))
+    else:
+        print(level+'-'+str(tree.col)+'-'+str(tree.val))
+        printf(tree.l,level+'L')
+        printf(tree.r,level+'R')
+
+
+def predict(test, tree):
+
+    if tree.sign != None:
+        return tree.sign
+
+    else:
+        testval = test[tree.col]
+        if testval >= tree.val:
+            branch = tree.r
+        else:
+            branch = tree.l
+        return predict(test, branch)
+
+
+if __name__ == '__main__':
+
+    data = np.array([[35, 176, 0, 20000, 0],
+                    [28, 178, 1, 10000, 1],
+                    [26, 172, 0, 25000, 0],
+                    [29, 173, 2, 20000, 1],
+                    [28, 174, 0, 15000, 1]])
+
+    tree = build(data, 'info')
+
+    test_sample = np.array([[24, 178, 2, 17000],
+                            [27, 176, 0, 25000],
+                            [27, 176, 0, 10000]])
+    
+    printf(tree,'O')
+    
+    # res=[]
+    # for var in test_sample:
+    #     res.append(predict(var,tree))
+    
+    # print(res)
---
+++ 聚类/test.py
@@ -0,0 +1,124 @@
+import numpy as np
+from numpy.linalg import norm 
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import pandas as pd
+from collections import Counter,defaultdict
+
+np.random.seed(0)
+def plus(data,k):
+    '''
+    这段代码 可以不用看 模拟kmean++的操作  用来尽可能分散初始位置
+    '''
+
+    cents=[]
+    cents.append(data[np.random.randint(len(data))])
+
+    for i in range(1,k):
+
+        dis=np.min(norm(data[:,None]-cents,axis=2),axis=1)
+        dis=np.power(dis,2)
+
+        prob=dis/dis.sum()
+        cum=np.cumsum(prob)
+
+        sign=np.random.rand()
+
+        for j,p in enumerate(cum):
+            if p>sign:
+                cents.append(data[j])
+                break
+    return np.array(cents)
+
+
+def kmean(data,k):
+
+    cents=plus(data,k)
+    m,n=data.shape
+
+    tags=np.zeros((m,2))
+
+    flag=True
+    while flag:
+        flag=False
+
+        distane=norm(data[:,None]-cents,axis=2)
+        dis=np.min(distane,axis=1)
+        tag=np.argmin(distane,axis=1)
+
+        if not (tags[:,0]==tag).all():
+            flag=True
+        tags[:,0]=tag
+        tags[:,1]=dis
+
+        df=pd.DataFrame(data)
+        cents=df.groupby(tag).apply(lambda x:x.mean(axis=0)).values 
+
+    return cents,tags
+
+def f(arr):
+
+    a,b=np.unique(arr,return_counts=1)
+    s=dict(zip(a,b))
+
+    return s
+
+def px(arr1,arr2):
+
+    s1=f(arr1)
+    s2=f(arr2)
+
+    k1=sorted(s1.items(),key=lambda x:x[1] )
+    k2=sorted(s2.items(),key=lambda x:x[1])
+
+    print(k1,k2)
+
+
+def score(label,tag):
+
+    counts=Counter(zip(label,tag))
+    dic=defaultdict(list)
+    
+    for var in np.unique(label):
+        for k,v in counts.items():
+            if k[0]==var:
+                dic[var].append([*k,v])
+    inps=[]
+    for vs in dic.values():
+        s=np.array(vs)
+        inps.append(s[np.argmax(s[:,2])])
+    
+    inps=np.array(inps)
+    maps=dict(zip(inps[:,0],inps[:,1]))
+
+    arr3=np.array(list(map(lambda x:maps[x],label)))
+
+    acc=(arr3==tag).sum()/len(tag)
+    print(acc)
+
+
+
+if __name__ == '__main__':
+
+    k=5 
+    data,label=make_blobs(500,2,centers=k,random_state=1)
+
+    cents,tags=kmean(data,k)
+    
+
+    # plt.scatter(data[:,0],data[:,1],c=label)
+    # plt.scatter(cents[:,0],cents[:,1],marker='*',color='r',s=75)
+    # plt.show()
+
+    
+    sk=pd.DataFrame({'label':label,'tag':tags[:,0].astype(np.int32)})
+
+    print(sk.head(10))
+
+    # px(label,tags[:,0])
+    score(label,tags[:,0])
+
+
+
+
+
